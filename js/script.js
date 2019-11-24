c553ba8d6a2e9360dc1ccbb3cfd62374c3460ddb
Jamie Gobeille <djent34@gmail.com>
2019-11-24 13:53:49 -0500

    added functionality to T-shirt infor section

--- js/script.js
+++ js/script.js
@@ -7,10 +7,10 @@
 const $name = $("#name");
 const $jobTitleOptions = $("#title");
 const $otherTitle = $("#other-title");
-const $tShirtOptions = $("#design");
-console.log($tShirtOptions);
-const colorOptions = $("#color option");
-console.log(colorOptions);
+const $designSelectElement = $("#design");
+const $designOptions = $("#design option");
+const $colorSelectElement = $("#color");
+const $colorOptions = $("#color option");
 
 //*============================================================================================
 
@@ -43,3 +43,47 @@ $jobTitleOptions.on("change", function() {
 //T-Shirt Info Section
 
 //*============================================================================================
+
+//Refactor this. This is seriously hot garbage
+
+//append select T-shirt text to the top of the list and make it selected
+$colorSelectElement.prepend(
+  '<option value="select" selected="selected"> Please select a T-shirt theme</option>'
+);
+
+//set all the color options to hidden
+$colorOptions.attr("hidden", true);
+
+//when the selection changes in design selection box, perform functions
+$designSelectElement.on("change", function(e) {
+  //hide the bolierplate text on change
+  $("#design > option:nth-child(1)").attr("hidden", true);
+  $("#color > option:nth-child(1)").attr("hidden", true);
+  //if target value is equal to js puns, then make the second option 'selected', show the first three options, and hide the rest
+  if ($(e.target).val() === "js puns") {
+    $("#color > option:nth-child(2)")
+      .attr("selected", true)
+      .show();
+    $("#color > option:nth-child(3)").show();
+    $("#color > option:nth-child(4)").show();
+
+    $("#color > option:nth-child(5)")
+      .attr("selected", false)
+      .hide();
+    $("#color > option:nth-child(6)").hide();
+    $("#color > option:nth-child(7)").hide();
+    //Else hide the first three options and make the last three visible
+  } else {
+    $("#color > option:nth-child(2)")
+      .attr("selected", false)
+      .hide();
+    $("#color > option:nth-child(3)").hide();
+    $("#color > option:nth-child(4)").hide();
+
+    $("#color > option:nth-child(5)")
+      .attr("selected", true)
+      .show();
+    $("#color > option:nth-child(6)").show();
+    $("#color > option:nth-child(7)").show();
+  }
+});
